c0bc1a22486945eaf9e8109a89c88cad2422020d
Leonardo <leonardocasia34@gmail.com>
2023-09-02 13:47:49 +0200

    Aggiunta Classe MatAggiungere i file di progetto.

---
+++ CalcWpf/FuncSMat/MatsMetod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation;
+
+namespace CalcWpf.FuncSMat
+{
+    class MatsMetod
+    {
+       
+        public static int somma(int a, int b)
+        {
+           return a + b;
+        }
+        public static float somma(float a, int b)
+        {
+            return a + b;
+        }
+        public static float somma(int a, float b)
+        {
+            return a + b;
+        }
+        public static float somma(float a, float b)
+        {
+            return a + b;          
+        }
+        public static decimal somma(int a, decimal b)
+        {
+            return a + b;
+        }
+        public static decimal somma(decimal a, int b)
+        {
+            return a + b;
+        }
+        public static decimal somma(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+    }
+}
