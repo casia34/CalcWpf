a183d6c41957998ed448d890f9cf678f2d7d2e5f
Leonardo <leonardocasia34@gmail.com>
2024-04-06 16:39:29 +0200

    Aggiornamento con nuove funzionalità e possibiltà di ascolate musica

--- CalcWpf/FuncSMat/MatsMetod.cs
+++ CalcWpf/FuncSMat/MatsMetod.cs
@@ -12,34 +12,21 @@ namespace CalcWpf.FuncSMat
     class MatsMetod
     {
        
-        public static int somma(int a, int b)
-        {
-           return a + b;
-        }
-        public static float somma(float a, int b)
-        {
-            return a + b;
-        }
-        public static float somma(int a, float b)
-        {
-            return a + b;
-        }
-        public static float somma(float a, float b)
-        {
-            return a + b;          
-        }
-        public static decimal somma(int a, decimal b)
-        {
-            return a + b;
-        }
-        public static decimal somma(decimal a, int b)
-        {
-            return a + b;
-        }
         public static decimal somma(decimal a, decimal b)
         {
             return a + b;
         }
-
+        public static decimal sottrazione(decimal a, decimal b)
+        {
+            return a - b;
+        }
+        public static decimal Divisione(decimal a, decimal b)
+        {
+            return a / b;
+        }
+        public static decimal Moltiplicazione(decimal a, decimal b)
+        {
+            return a * b;
+        }
     }
 }
--- CalcWpf/MainWindow.xaml.cs
+++ CalcWpf/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 
-﻿using CalcWpf.FuncSMat;
+using CalcWpf.FuncSMat;
 using System;
 
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -35,36 +37,82 @@ namespace CalcWpf
         public MainWindow()
         {
             InitializeComponent();
+            
+          var FileNames = Directory.GetFiles( Assembly.GetAssembly(typeof(MatsMetod)).Location.Split("bin")[0] + @"Canzoni");
+         
         }
 
-     
 
-       
 
-        private void NumberOne_Click(object sender, RoutedEventArgs e)
+
+        private void AddNumber_Click(object sender, RoutedEventArgs e)
         {
+            Button buttonEventGener = (Button)sender;
             if (flagToUseTheSecondVar)
             {
-                ValueB.Text += "1";
+                ValueB.Text += buttonEventGener.Content;
             }
             else
             {
-                ValueA.Text += "1";
+                ValueA.Text += buttonEventGener.Content;
             }
-           
+
         }
 
         private void Result_Click(object sender, RoutedEventArgs e)
         {
-            ValueA.Text = MatsMetod.somma(int.Parse(ValueA.Text),int.Parse(ValueB.Text)).ToString();
+            if (Operetor.Text == "+")
+            {
+                ValueA.Text = MatsMetod.somma(decimal.Parse(ValueA.Text), decimal.Parse(ValueB.Text)).ToString();
+
+            }else if (Operetor.Text == "-")
+            {
+                ValueA.Text = MatsMetod.sottrazione(decimal.Parse(ValueA.Text), decimal.Parse(ValueB.Text)).ToString();
+
+            }
+            else if (Operetor.Text == "*")
+            {
+                ValueA.Text = MatsMetod.Moltiplicazione(decimal.Parse(ValueA.Text), decimal.Parse(ValueB.Text)).ToString();
+
+            }
+            else if (Operetor.Text == "/")
+            {
+                ValueA.Text = MatsMetod.Divisione(decimal.Parse(ValueA.Text), decimal.Parse(ValueB.Text)).ToString();
+
+            }
+
             ValueB.Text = "";
             Operetor.Text = "";
         }
 
         private void Piu_Button_Click(object sender, RoutedEventArgs e)
         {
+
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+            player.SoundLocation = @"..\\Canzoni\\Daft Punk - One More Time (Official Video).wav";
+            player.Play();
+
             flagToUseTheSecondVar = true;
             Operetor.Text += "+";
+        }
+
+        private void Meno_Button_Click(object sender, RoutedEventArgs e)
+        {
+            flagToUseTheSecondVar = true;
+            Operetor.Text += "-";
+        }
+
+        private void divso_button_Click(object sender, RoutedEventArgs e)
+        {
+            flagToUseTheSecondVar = true;
+            Operetor.Text += "/";
+        }
+
+        private void Moltiplicazione_Button_Click(object sender, RoutedEventArgs e)
+        {
+            flagToUseTheSecondVar = true;
+            Operetor.Text += "*";
         }
     }
 }
